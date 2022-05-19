3b27a5f3275e3571a538120a41b87c3a26e3188d
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-19 18:11:53 +0530

    Added: Adding Ifelse program.

---
+++ src/com/bridglabz/IfElse.java
@@ -0,0 +1,16 @@
+package com.bridglabz;
+
+import java.sql.SQLOutput;
+
+public class IfElse {
+    public static void main(String[] args) {
+    int x=22;
+    int y=15;
+        if (x > y){
+        System.out.println("x is greater than y");
+    }
+        else {
+            System.out.println("Y is greater.");
+        }
+        }
+}
